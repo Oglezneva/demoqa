ff597848486bb6eeef940e0159fa81d58be4136b
Oglezneva <oglezneva@yahoo.com>
2023-05-11 19:51:50 +0300

    first commit

---
+++ pages/modal_dialogs.py
@@ -0,0 +1,11 @@
+from pages.base_page import BasePage
+from components.components import WebElements
+
+class ModalDialogs(BasePage):
+
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/modal-dialogs'
+        super().__init__(driver, self.base_url)
+
+        self.btns_third_menu = WebElements(driver, 'div:nth-child(3) > div > ul > li')
+        self.main_icon = WebElements(driver, '#app > header > a > img')
---
+++ tests/tests_hw/test_page_dialogs.py
@@ -0,0 +1,26 @@
+from pages.modal_dialogs import ModalDialogs
+from pages.demoqa import DemoQa
+
+
+def test_modal_elements(browser):
+    modal_dialogs = ModalDialogs(browser)
+
+    modal_dialogs.visit()
+    assert modal_dialogs.btns_third_menu.check_count_elements(5)
+
+
+def test_navigation_modal(browser):
+    modal_dialogs = ModalDialogs(browser)
+    demo_qa_page = DemoQa(browser)
+
+    modal_dialogs.visit()
+    browser.refresh()
+    modal_dialogs.main_icon.click()
+    browser.back()
+    browser.set_window_size(900, 400)
+    browser.forward()
+    assert demo_qa_page.equal_url()
+    assert browser.title == demo_qa_page.pageData['title']
+    browser.set_window_size(1000, 1000)
+
+
