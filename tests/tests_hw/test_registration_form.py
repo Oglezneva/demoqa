05683150cd546e6f5ad987c6ee08943f1d653ecc
Oglezneva <oglezneva@yahoo.com>
2023-05-19 18:57:56 +0300

    first commit

--- tests/tests_hw/test_registration_form.py
+++ tests/tests_hw/test_registration_form.py
@@ -17,15 +17,32 @@ def test_registration_form(browser):
     web_tables.salary.send_keys('3000')
     web_tables.department.send_keys('development')
     web_tables.btn_submit.click()
-    assert web_tables.new_first_name.get_text() == 'Alena'
+    assert web_tables.new_first_name.get_text() == 'oglezneva@yahoo.com'
     web_tables.pencil.click()
     assert web_tables.modal_content.exist()
-    web_tables.first_name.clear()
-    web_tables.first_name.send_keys('Maria')
+    web_tables.email.clear()
+    web_tables.email.send_keys('ololosha@yahoo.com')
     web_tables.btn_submit.click()
-    assert web_tables.new_first_name.get_text() == 'Maria'
+    assert web_tables.new_first_name.get_text() == 'ololosha@yahoo.com'
     web_tables.delete.click()
-    assert not web_tables.new_first_name.get_text() == 'Alena'
+    assert not web_tables.new_first_name.get_text() == 'ololosha@yahoo.com'
 
 
-def
+def test_pagination(browser):
+    web_tables = WebTables(browser)
+
+    web_tables.visit()
+    web_tables.rows.scroll_to_element()
+    web_tables.rows.click()
+    web_tables.pagination_option_5.click()
+
+
+
+
+
+
+
+
+
+
+
